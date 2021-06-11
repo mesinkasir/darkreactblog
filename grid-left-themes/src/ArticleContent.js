190cc3a239bde110fc2db3f9733533b78144f76d
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2021-06-11 16:18:41 +0700

    Gird Left Themes
    Upload new grid left themes template react web blog

---
+++ grid-left-themes/src/ArticleContent.js
@@ -0,0 +1,125 @@
+const articles = [
+  {
+    name: 'about',
+    title: 'About Us',
+    desc: 'Dark react team dev',
+    img:
+      'https://1.bp.blogspot.com/-r-wSGgqtsm0/X-Hr184zSbI/AAAAAAAAL-8/I377EooVg4gg4PMyPfh4PBIcKU0Kf83zQCLcBGAsYHQ/s625/pembuatan%2Bwebsite%2Bcompany%2Bprofile%2B%25283%2529.png',
+    content: [
+      `Dark react website is a free and open source code, you can change article and code with your own, develope by axcora team technology for help and support developer build new modern website using react with blogging platform.`
+    ]
+  },
+  {
+    name: 'install',
+    title: 'How to Install',
+    desc: 'Installation react dark themes',
+    img:
+      'https://1.bp.blogspot.com/-4SJl3zxEmI8/X-MC7eawLPI/AAAAAAAAMDk/H_5XbaABIowyhxS547xBFZ3rP6o0rSOsgCLcBGAsYHQ/s626/APK%2Bandroid%2Bweb%2Bsekolah.jpg',
+    content: [
+      `For first you need install third party apps, download node js npm on https://nodejs.org/en/download/.`,
+      `Then install on your device, after installation success now you can download react dark blog themes template for free, download or fork it, and we need work with node for installation , just run`,
+      `npm install`,
+      `npm start`,
+      `for build and upload host just run command`,
+      `npm run build`,
+      `then upload on your host`
+    ]
+  },
+  {
+    name: 'work',
+    title: 'Work with dark react',
+    desc: 'How to update and create article blog',
+    img:
+      'https://1.bp.blogspot.com/-CaGGNz-9VSk/X-MC0VL9YcI/AAAAAAAAMDc/3FvrjM18IuwZ5f7LRWJmRPuPc-L3eoZrwCLcBGAsYHQ/s626/apl.jpg',
+    content: [
+      `For first you need to open source code , then visit on index.js , ArticleList.js file and change SEO with your needed, after change all SEO, now you can create and update article content, just visit on ArticleContent.js and follow or copy paste code fore create new article content, or change article with you needed.`
+    ]
+  },
+  {
+    name: 'blogs',
+    title: 'Dark React Blogs',
+    desc: 'Free react website themes template',
+    img:
+      'https://a.fsdn.com/con/app/proj/pembuatanweb/screenshots/free%20download%20react%20website%20themes%20template%20%285%29.png/max/max/1',
+    content: [
+      `React website template with blog area , including animation in design and unique , download now for free. `,
+      `Github repo : https://github.com/mesinkasir/darkreactblog.`,
+      `Sourceforge : https://sourceforge.net/projects/pembuatanweb/.`,
+      `Demo : https://kasironline.pages.dev.`,
+      `Demo 2 : https://sidoarjo.pages.dev/`,
+      `Premium version : https://surabaya.pages.dev/`
+    ]
+  },
+  {
+    name: 'card',
+    title: 'Dark React Card',
+    desc: 'Free react website themes template V.2 with card',
+    img:
+      'https://a.fsdn.com/con/app/proj/pembuatanwebsite/screenshots/react%20website%20template%20themes%20free%20download%20%281%29.png/max/max/1',
+    content: [
+      `Free download react website template themes with card version. download now for free. `,
+      `Github repo : https://github.com/mesinkasir/darkreactblog/tree/main/cardthemes`,
+      `Sourceforge : https://sourceforge.net/projects/pembuatanwebsite/`,
+      `Demo : https://axcora.pages.dev/`,
+      `Premium version : https://reactindo.pages.dev/`
+    ]
+  },
+  {
+    name: 'slide',
+    title: 'Slide Premium themes',
+    desc: 'React website template themes premium',
+    img:
+      'https://1.bp.blogspot.com/-HeGvPQzJt6Y/YMI6Pmp9OKI/AAAAAAAAPIo/L9AQ-dL_olstJBvHjwlTkr8uSegodrkYACLcBGAsYHQ/s1348/free%2Breact%2Bwebsite%2Bdownload%2B%25282%2529.png',
+    content: [
+      `Premium themes for dark react, you can make donation just only $.10 on our bank account,cek demo https://surabaya.pages.dev/ , We accept online payment using money transfer , so you can order by using moneygram or western union to sent money, 
+after transaction success you can use your web app. and active by default for one years, 
+use money transfer like western union or moneygram and sent order to we bank account.`,
+      `DONATE : $ 10`,
+      `BANK CENTRAL ASIA`,
+      `ACCOUNT NO : 0181884109`,
+      `ACCOUNT NAME:  SUCI CHANIFAH`,
+      `IBAN/SWIFT CODE : CENAIDJA.`
+    ]
+  },
+
+  {
+    name: 'premium',
+    title: 'Card Premium themes',
+    desc: 'React website template themes premium',
+    img:
+      'https://1.bp.blogspot.com/-LERGwh6pYwA/YMI6PtKtB3I/AAAAAAAAPIs/vr41HLxJacwsnQlickjZY6lfBJGd3_2uwCLcBGAsYHQ/s1349/free%2Breact%2Bwebsite%2Bdownload%2B%25283%2529.png',
+    content: [
+      `Premium themes for dark react, you can make donation just only $.10 on our bank account,cek demo on https://reactindo.pages.dev/ , We accept online payment using money transfer , so you can order by using moneygram or western union to sent money, 
+after transaction success you can use your web app. and active by default for one years, 
+use money transfer like western union or moneygram and sent order to we bank account.`,
+      `DONATE : $ 10`,
+      `BANK CENTRAL ASIA`,
+      `ACCOUNT NO : 0181884109`,
+      `ACCOUNT NAME:  SUCI CHANIFAH`,
+      `IBAN/SWIFT CODE : CENAIDJA.`
+    ]
+  },
+
+  {
+    name: 'bonus',
+    title: 'Bonus Source Code',
+    desc: 'React POS Point of sale.',
+    img:
+      'https://camo.githubusercontent.com/7c48d762a8e473a15d006d4c225aafbef17a615eef375db9ea3f196f9c3a811a/68747470733a2f2f612e6673646e2e636f6d2f636f6e2f6170702f70726f6a2f7265616374706f732f73637265656e73686f74732f7265616374706f73253230706f696e742532306f6625323073616c652532306275696c642532307769746825323072656163742e706e672f6d61782f6d61782f31',
+    content: [
+      `Special bonus react point of sale apps, for make fast cashier transaction, donwload now for free https://github.com/mesinkasir/reactpos`
+    ]
+  },
+  {
+    name: 'developewebsite',
+    title: 'Web Dev',
+    desc: 'Website design development services.',
+    img:
+      'https://1.bp.blogspot.com/-eXysooc8tP8/X-MC99LYS2I/AAAAAAAAMD4/wQO1XZDgdcgCLkZxx4byL5JE9xkFQ4JfQCLcBGAsYHQ/s497/web_hosting.png',
+    content: [
+      `If you need , you can our services for develop modern website using react js with cheap price , contact our team for information details`
+    ]
+  }
+];
+
+export default articles;
---
+++ grid-left-themes/src/Contact.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './Wohoo';
+const Contact = () => (
+  <div className="text-center uk-animation-toggle">
+    <hr />
+    <a
+      href="tel:+6285331361404"
+      className="btn btn-outline-primary rounded-pill"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        className="bi bi-headset"
+        viewBox="0 0 16 16"
+      >
+        <path d="M8 1a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V6a6 6 0 1 1 12 0v6a2.5 2.5 0 0 1-2.5 2.5H9.366a1 1 0 0 1-.866.5h-1a1 1 0 1 1 0-2h1a1 1 0 0 1 .866.5H11.5A1.5 1.5 0 0 0 13 12h-1a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h1V6a5 5 0 0 0-5-5z" />
+      </svg>{' '}
+      Call us
+    </a>
+    <a
+      href="https://wa.me/6285646104747"
+      className="btn btn-outline-success rounded-pill"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        className="bi bi-chat"
+        viewBox="0 0 16 16"
+      >
+        <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z" />
+      </svg>{' '}
+      Whatsapp
+    </a>
+    <a
+      href="https://github.com/mesinkasir/darkreactblog"
+      className="btn btn-outline-light rounded-pill"
+      target="_blank"
+    >
+      Github
+    </a>
+    <hr />
+  </div>
+);
+
+export default Contact;
